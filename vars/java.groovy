6a32530c899725d87ef87aeebd9a8212d2f62d17
Halit Soydemir <halit.soydemir@iyzico.com>
2020-05-12 16:11:38 +0300

    added shared lib

---
+++ vars/java.groovy
@@ -0,0 +1,43 @@
+def call(body) {
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    pipeline {
+        agent any
+        stages {
+            stage('checkout git') {
+                steps {
+                    git branch: pipelineParams.branch, credentialsId: 'GitCredentials', url: pipelineParams.scmUrl
+                }
+            }
+
+            stage('build') {
+                steps {
+                    sh 'mvn clean package -DskipTests=true'
+                }
+            }
+
+            stage ('test') {
+                steps {
+                    parallel (
+                        "unit tests": { sh 'mvn test' },
+                        "integration tests": { sh 'mvn integration-test' }
+                    )
+                }
+            }
+
+            stage('deploy developmentServer'){
+                steps {
+                    sh 'echo deploying...'
+                }
+            }
+        }
+        post {
+            failure {
+                mail to: pipelineParams.email, subject: 'Pipeline failed', body: "${env.BUILD_URL}"
+            }
+        }
+    }
+}
