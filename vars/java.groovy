ca1176938d49fd628a1690163afdfd2e4bb3e68d
Halit <31814411+halitsoydemir@users.noreply.github.com>
2020-05-20 13:12:17 +0300

    Update java.groovy

--- vars/java.groovy
+++ vars/java.groovy
@@ -60,11 +60,11 @@ volumes: [
 
           sh """
 
-            docker build -t registry.iyzico.net/halit.soydemir/${pipelineParams.registeryName}:${env.BUILD_NUMBER} .
+            docker build -t registry.iyzico.net/iyzico/iyzipay/${pipelineParams.registeryName}:${env.BUILD_NUMBER} .
 
             docker images
             
-            docker push registry.iyzico.net/halit.soydemir/${pipelineParams.registeryName}:${env.BUILD_NUMBER}
+            docker push registry.iyzico.net/iyzico/iyzipay/${pipelineParams.registeryName}:${env.BUILD_NUMBER}
 
             """
 
